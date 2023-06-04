aaa5ee14f3fd41ac13a4d3f2dac33c2825e2092b
Emre Sulukan <esulukan34@gmail.com>
2023-06-04 18:39:30 +0300

    finish

--- cypress/e2e/Crea/edvisorly/Login.cy.js
+++ cypress/e2e/Crea/edvisorly/Login.cy.js
@@ -39,55 +39,56 @@ describe('Login Test', () => {
         let lastName = faker.name.lastName()
         cy.get('[name="lastName"]').clear().type(lastName) 
 
-        //let dogumTarihi = faker.date.birthdate()
+        // 10) Dogum tarihi gir
         cy.get('.react-datepicker__input-container > .form-control').clear().type('05/05/1990')
         cy.get('.react-datepicker__day--005').click()
 
-        // Pronauns
+        //11) Pronauns
         cy.get('.form-select').select(3)
 
-        // State
+        // 12) State sec
         cy.get('.css-19bb58m').first().click()
         cy.wait(4000)
-        cy.contains('Virginia').scrollIntoView({duration:5000})
+        cy.contains('Virginia').scrollIntoView({duration:1000})
         cy.contains('Virginia').click({force: true})
         
-        // Save gorene kadar asagi in
-        cy.get('.d-flex > .btn').scrollIntoView({duration:5000})
-        // Collage
+        // 13) Save gorene kadar asagi in
+        cy.get('.d-flex > .btn').scrollIntoView({duration:1000})
+        // 14) Collage butonuna tikla
         cy.get('.css-19bb58m').eq(1).click()
         cy.wait(4000)
-        //cy.contains('Union County College').scrollIntoView({duration:5000})
+        // 15) Collage secimi yap
         cy.contains('Danville Community College').click({force: true})
 
-        // Save butonuna bas
+        // 16) Save butonuna bas
         cy.get('.d-flex > .btn').click()
 
-        // Degisiklerin kayit edildigini dogrula
+        // 17) Degisiklerin kayit edildigini dogrula
         cy.wait(2000)
         cy.on('window:alert', ()=>{
             cy.get('.Toastify__toast-body > :nth-child(2)').should('include.text', 'Profile updated successfully!')
         })
-
         cy.wait(4000)
-        // 6) Avatara tikla
+
+        // 18) Avatara tikla
         cy.get('.Navbar_navItem__g_Z_i').eq(2).click()
 
-        // 7) Profile tikla
+        // 19) Profile tikla
         cy.get('.dropdown-menu > :nth-child(1)').click()
 
-        
-        // Bio ya tikla
+
+        // 20) Bio ya tikla
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .first().click()
         cy.get('.ProfileDrawer_editArea__ViYx3 > .form-control').clear({force: true}).type('Yazilim Test Muhendisiyim')
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .first().click()
-        // Yazinin degistigini dogrula
+
+        // 21) Yazinin degistigini dogrula
         cy.contains('Yazilim Test Muhendisiyim').should('have.text', 'Yazilim Test Muhendisiyim')
         cy.wait(2000)
 
-        // Identifier a tikla
+        // 22) Identifier a tikla
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .eq(1).click()
         cy.wait(2000)
@@ -95,20 +96,17 @@ describe('Login Test', () => {
         cy.get('div.Dropdown_dropdown__B_WVz svg[aria-hidden="true"]')
         .eq(7).click()
 
-        // Faith-Based e tikla
-        //cy.get('div.Dropdown_dropdownMenuItem__4Bmq_ label[for=":r0:- Faith-Based"]').click()
+        // 23) Faith-Based e tikla
         cy.get('.Dropdown_dropdownLabel__FEKRu.form-check-label>input')
         .eq(0).check()
         cy.wait(3000)
 
-        // First Gen Tikla
-        //cy.get('div.Dropdown_dropdownMenuItem__4Bmq_ label[for=":r1:- First-Gen"]').click()
+        // 24) First Gen Tikla
         cy.get('.Dropdown_dropdownLabel__FEKRu.form-check-label>input')
         .eq(1).uncheck()
 
 
-        // Veteran a tikla
-        //cy.get('div.Dropdown_dropdownMenuItem__4Bmq_ label[for=":ra:- Veteran"]').click()
+        // 25) Veteran a tikla
         if(cy.get('.Dropdown_dropdownLabel__FEKRu.form-check-label>input').eq(10).should('be.checked')){
         // Apply butonuna bas
             cy.get('div.Dropdown_dropdownMenuFooter__aeLrh button').eq(1).click()
@@ -123,63 +121,48 @@ describe('Login Test', () => {
            
         }
         
-        // Faith-Based, Parent, Veteran degistigini dogrula
+        // 26) Faith-Based, Parent, Veteran oldugunu dogrula
         cy.contains('Faith-Based, Parent, Veteran').should('have.text', "Faith-Based, Parent, Veteran")
         
-        // Ethnicity a tikla
+        // 27) thnicity a tikla
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .eq(2).click()
         cy.wait(3000)
         cy.get('.Dropdown_dropdown__B_WVz.dropdown svg[aria-hidden="true"]')
         .eq(7).click()
 
-        //White butonuna tikla
+        // 28) White butonuna tikla
         cy.get('div>div.Dropdown_dropdownMenuItem__4Bmq_.form-check')
         .eq(5).click()
 
-        // American'i tikla
+        // 29) American'i tikla
         cy.get('div>div.Dropdown_dropdownMenuItem__4Bmq_.form-check')
         .eq(0).click()
 
-        // Apply butonuna bas
+        // 30) Apply butonuna bas
         cy.get('div.Dropdown_dropdownMenuFooter__aeLrh>button')
         .eq(1).click()
 
-        // American Indian or Alaska Native degistigini dogrula
+        // 31) American Indian or Alaska Native degistigini dogrula
         cy.contains('American Indian or Alaska Native').should('have.text', "American Indian or Alaska Native")
 
-        // Work Status a tikla
+        // 32) Work Status a tikla
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .eq(4).click()
         cy.wait(2000)
 
-        // Full Time sec
+        // 33) Full Time sec
         cy.get('.form-select').select('Full Time')
 
-        // Wrok Employer
-        cy.get('.ProfileDrawer_editArea__ViYx3>input.form-control').clear().type('amazon')
+        // 34) Wrok Employer 'Crea' yaz
+        cy.get('.ProfileDrawer_editArea__ViYx3>input.form-control').clear().type('Crea')
 
-        // Onay butonu tikla
+        // 35) Onay butonu tikla
         cy.get('div.ProfileDrawer_body__rN2sJ svg[stroke=currentColor]')
         .eq(4).click()
 
-        // Yazinin degistigini dogrula
-        cy.contains('Full Time at amazon').should('have.text', 'Full Time at amazon')
-
-
-
-
-
-
-
-
-
-
-        
-        
+        // 36) Yazinin degistigini dogrula
+        cy.contains('Full Time at Crea').should('have.text', 'Full Time at Crea')
 
     })
-
-    
-
 })
